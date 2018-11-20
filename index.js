1a0388b23689bd3e38574d523753edc4dcc2b74b
Svetlana Linuxenko <svetlana@linuxenko.pro>
2018-11-20 22:57:14 +0200

    Logging in multiple emails

--- index.js
+++ index.js
@@ -2,26 +2,64 @@
 * File Name     : index.js
 * Created By    : Svetlana Linuxenko, <svetlana@linuxenko.pro>, www.linuxenko.pro
 * Creation Date : [2018-11-20 15:24]
-* Last Modified : [2018-11-20 20:23]
+* Last Modified : [2018-11-20 22:56]
 * Description   :  
 **********************************************************************************/
 
-const Tagged = require('./lib/tagged');
-const cookie = 'B=b=DA2BE4A7F59BBC2C&locale_cookie=en_US&remember_me=; _ga=GA1.2.1944660281.1536063691; G_ENABLED_IDPS=google; _gid=GA1.2.926165895.1537252582; __qca=P0-391567704-1538331239367; __gads=ID=569a20580ae5ae15:T=1538331240:S=ALNI_Mb1QsV4QQy6cMXjSWQaTmy59zldUw; __zlcmid=p6hkXaUVGXdrtu; S=n509p8jajf19a36gbajot64kbh; L=3iUAXEFn9MET.1rZ27s.5Fzzvq; _gat_UA-1982760-1=1';
+require('dotenv').config();
+const EMAILS = process.env.EMAILS.split(',');
+const PROXIES = process.env.PROXIES.split(',');
 
-const fs = require('fs');
+const assert = require('assert');
+
+const Storage = require('node-storage');
+const db = new Storage(process.env.DB || __dirname + './data/db');
+db.put('checkStore', true);
+assert(db.get('checkStore'), 'Storage does not work');
+
+const Tagged = require('./lib/tagged');
+const login = require('./lib/login');
+
+async function startSession(email, proxy) {
+}
 
 (async function() {
-  const proxy = 'socks5://127.0.0.1:9000';
-  let tagged = new Tagged({ proxy, cookie });
 
-  try {
+  for (let idx in EMAILS) {
+    let email = EMAILS[idx];
+    let proxy = PROXIES[idx];
+    let cookie = db.get(`${email}.cookie`) || await login({ email, password: process.env.ALLPASS, proxy });
+    let tagged = new Tagged({ proxy, cookie });
+    let id = await tagged.myId();
+
+    if (id) {
+      if (process.env.DEBUG) {
+        console.log('Successfully logged in by ', id);
+       }
+    } else {
+      cookie = await login({ email, password: process.env.ALLPASS, proxy });
+    }
+
+    db.put(`${email}.cookie`, cookie);
+  }
+
+//  let tagged = new Tagged({ proxy, cookie });
+ 
+
+//  try {
+//    console.log(await login({
+//      email: 'julia.r12@gmail.su',
+//      password: 'lnYZbc6B4taWj9Cd',
+//      proxy
+//    }));
 //    console.log(await tagged.petInfo('5458689216'));
 //    console.log((await tagged.getPets2()));
-    console.log(await tagged.myId());
-  } catch(e) {
-    console.log(e);
-  }
+//   let petId = '6066301549';
+//   let pet = (await tagged.petInfo(petId)).results.pet; 
+//   console.log(await tagged.buy(pet));
+//  } catch(e) {
+//    console.log(e);
+//  }
 //  console.log(await tagged.wishList());
 //  console.log(await tagged.pip());
   //
