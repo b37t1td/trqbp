3dd59660444cea65d9ca4bfdb73e3684db255289
Svetlana Linuxenko <svetlana@linuxenko.pro>
2018-11-20 19:12:18 +0200

    More adequate pip with headers and stuff

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 * File Name     : index.js
 * Created By    : Svetlana Linuxenko, <svetlana@linuxenko.pro>, www.linuxenko.pro
 * Creation Date : [2018-11-20 15:24]
-* Last Modified : [2018-11-20 18:46]
+* Last Modified : [2018-11-20 18:57]
 * Description   :  
 **********************************************************************************/
 
@@ -12,6 +12,6 @@ const Tagged = require('./lib/tagged');
   const proxy = 'socks5://127.0.0.1:9000';
   let tagged = new Tagged({ proxy });
 
-  console.log(`Starting from: ${(await tagged.pip())}`);
+  console.log(await tagged.pip());
 
 })();
