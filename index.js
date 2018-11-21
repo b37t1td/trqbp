7ccb92c5f1fbea2e71548fed5d28c2b613b7bab9
Svetlana Linuxenko <svetlana@linuxenko.pro>
2018-11-21 12:41:24 +0200

    Crawler first commit

--- index.js
+++ index.js
@@ -2,92 +2,81 @@
 * File Name     : index.js
 * Created By    : Svetlana Linuxenko, <svetlana@linuxenko.pro>, www.linuxenko.pro
 * Creation Date : [2018-11-20 15:24]
-* Last Modified : [2018-11-21 02:38]
+* Last Modified : [2018-11-21 05:35]
 * Description   :  
 **********************************************************************************/
 
 require('dotenv').config();
-const EMAILS = process.env.EMAILS.split(',');
-const PROXIES = process.env.PROXIES.split(',');
-
 const { startInstance } = require('./lib/poll');
 const Remote = require('./lib/remote');
+const BigNumber = require('bignumber.js');
+
+const LP = BigNumber('9146620701612724868483225687720398878210090213855');
+const BP = BigNumber('9235568581687470146620701612724868483225687720398878210090213855');
+
+const Storage = require('node-storage');
+const db = new Storage(process.env.DB + '-polls');
+
+function wids(data) {
+  return data.results.pets.map(function(d) {
+    return d.userId;
+  });
+}
+
+function validate(e) {
+  let pp = BigNumber(e.purchase_price);
+
+  if (e.event_type === 3 && (new Date() - new Date(e.event_date * 1000)) < 2420000) {
+    if (pp.isGreaterThan(LP) && pp.isLessThan(BP)) {
+      return e.pet_id;
+    }
+  }
+}
+
+function sleep(millis) {
+  return new Promise(resolve => setTimeout(resolve, millis));
+}
 
 (async function() {
   let bots = [];
-
-  function findBot(id) {
-    return bots.filter((b) => b.id === id)[0];
-  }
-
   const remote = new Remote('wss://app-plqkqftgch.now.sh', function(data) {
-    function remoteSync(bot) {
-      process.nextTick(() => {
-        remote.send({ type: 'force-pong', id: bot.id, stats: bot.petRuns(), state: true });
-      });
-    }
-
-    if (data.type === 'share') {
-      for (let bot of bots) {
-        bot.run(String(data.id), data.price);
-        remoteSync(bot);
-       }
-      return;
-    }
-
-    if (data.type === 'remove') {
-      for (let bot of bots) {
-        bot.remove(String(data.id));
-        remoteSync(bot);
-       }
-      return;
-    }
-
-    if (data.type === 'ping') {
-      process.nextTick(() => {
-        for (let bot of bots) {
-          remote.send({ type: 'pong', id: bot.id, stats: bot.petRuns() });
-         }
-      });
-      return;
-    }
-
-    if (data.type === 'buy-remote') {
-      let bot = findBot(data.client);
-      if (bot) {
-        bot.petInfo(data.pet).then(function(pet) {
-          bot.buy(pet);
-        });
-        remoteSync(bot);
-      }
-      return;
-    }
-
-    if (data.type === 'run-remote') {
-      let bot = findBot(data.client);
-      if (bot) {
-        bot.run(String(data.id), data.price);
-        remoteSync(bot);
-      }
-      return;
-    }
-
-    if (data.type === 'remove-remote') {
-      let bot = findBot(data.client);
-      if (bot) {
-        bot.remove(String(data.pet));
-        remoteSync(bot);
-      }
-      return;
+    if (data.type === 'pongs') {
+      bots = data.pongs;
     }
   });
 
   try {
-    for (let idx in EMAILS) {
-      bot = await startInstance(EMAILS[idx], PROXIES[idx]);
-      bot.id = await bot.myId();
-      bots.push(bot);
+    let bot = await startInstance('pam.r12@gmail.su', 'socks://127.0.0.1:9007');
+    let wishes = wids(await bot.wishList({ num_items: 100 }));
+
+    setInterval(async function() {
+      wishes = wids(await bot.wishList({ num_items: 100 }));
+    }, 200000);
+
+    async function crawl() {
+      for (let w of wishes) {
+        let e = (await bot.news({ id: w, num_events: 1})).results.events_html[0];
+
+        if (e) {
+          let id = validate(e);
+          if (id) {
+            let uuid = id + '-' + w;
+            if (!db.get(uuid)) {
+              db.put(uuid, true);
+              console.log('stored', uuid);
+              let b = bots[Math.floor(Math.random() * bots.length)];
+              remote.send({ type: 'run-remote', client: b.id, id: Number(id), price: '10' });
+            }
+          }
+        }
+      }
+
+     await sleep(7000);
+     crawl();
     }
+
+    crawl();
+
   } catch(e) {
     console.log(e);
   }
