ff7f989191344d654fa6001ca2173cfdfbe8c02c
Svetlana Linuxenko <svetlana@linuxenko.pro>
2018-11-22 01:24:11 +0200

    Catches with logging and return

--- tools/utils.js
+++ tools/utils.js
@@ -2,11 +2,15 @@
 * File Name     : tools/utils.js
 * Created By    : Svetlana Linuxenko, <svetlana@linuxenko.pro>, www.linuxenko.pro
 * Creation Date : [2018-11-20 15:44]
-* Last Modified : [2018-11-20 15:46]
+* Last Modified : [2018-11-22 01:22]
 * Description   :  
 **********************************************************************************/
 
 module.exports.trim = function(text) {
   return text.replace(/\s/gi, '');
+}
+
+module.exports.sleep = function (millis) {
+  return new Promise(resolve => setTimeout(resolve, millis));
 }
 
